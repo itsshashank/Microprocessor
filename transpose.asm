a0e5eecb5bde6ad167a1db6c3cd3342405403d78
Shashank Babu <shashank.sj1@programmer.net>
2019-04-07 21:22:57 +0530

    the right way

--- transpose.asm
+++ transpose.asm
@@ -1,33 +1,89 @@
 data segment
-mat db 1h,2h,3h,4h,5h,6h,7h,8h,9h
+    MSG1 DB "MATRIX",0AH,24H
+    MSG2 DB "AFTER TRANSPOSE",0AH,24H
+    M db 1,2,3,4
+    n db 4 dup()
+    nl db 0ah
 data ends
 code segment
 assume cs:code,ds:data
 start:
-mov ax,data
-mov ds,ax
-mov bl,03h
-mov cl,03h
-mov si,offset mat
-xor ax,ax
-next:
-	mov al,[si]
-	add si,3
-	MOV Dl,al
-	ADD Dl,30H
-	MOV AH,02H
-	INT 21H
-	loop next
-	sub si,09h
-	dec bl
-	cmp bl,0h
-	jz exit
-again:
-	inc si
-	mov cl,03h
-	jmp next
-exit:
-	mov ah,4ch
-	int 21h
+    mov ax,data
+    mov ds,ax
+
+    mov bl,2
+    mov cl,2
+    mov si,offset M
+    xor ax,ax
+    MOV AH,09H
+    MOV DX,offset MSG1
+    INT 21h
+
+    mrow:
+        mov dl,[si]
+        inc si
+        add dl,30h
+        mov ah,02h
+        int 21h
+        loop mrow
+        dec bl
+        jz tra
+    mcol:
+        mov ah,02h
+        mov dl,nl
+        int 21h
+        mov cl,2
+        jmp mrow
+    tra:
+    mov ah,02h
+    mov dl,nl
+    int 21h
+    mov bl,2
+    mov cl,2
+    mov si,offset M
+    mov di,offset n
+    xor ax,ax
+    row:
+        mov al,[si]
+        add si,2
+        mov [di],al
+        inc di
+        loop row
+        sub si,4
+        dec bl
+        cmp bl,0
+        jz printn
+    col:
+        inc si
+        mov cl,2
+        jmp row
+    printn:
+    mov bl,2
+    mov cl,2
+    mov si,offset n
+    xor ax,ax
+    MOV AH,09H
+    MOV DX,offset MSG2
+    INT 21h
+    
+    nrow:
+        mov dl,[si]
+        inc si
+        add dl,30h
+        mov ah,02h
+        int 21h
+        loop nrow
+        dec bl
+        jz skip
+    ncol:
+        mov ah,02h
+        mov dl,nl
+        int 21h
+        mov cl,2
+        jmp nrow
+
+    skip:
+    mov ah,4ch
+    int 21h
 code ends
 end start
